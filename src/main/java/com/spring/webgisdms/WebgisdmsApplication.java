f6a994bfe9eea3b059824c5c2e6ec29e3a7a39c0
gd <839036951@qq.com>
2024-08-03 18:23:30 +0800

    first commit

---
+++ src/main/java/com/spring/webgisdms/WebgisdmsApplication.java
@@ -0,0 +1,15 @@
+package com.spring.webgisdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebgisdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgisdmsApplication.class, args);
+
+		System.out.println("hello spring");
+	}
+
+}
---
+++ src/test/java/com/spring/webgisdms/WebgisdmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.webgisdms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebgisdmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
